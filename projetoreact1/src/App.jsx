d9092052408f9c253ef01a94eb5808bd837adec6
MHenrique2004 <mhenriquesouza983@gmail.com>
2023-10-09 21:16:50 -0300

    Primeiro Commit

---
+++ projetoreact1/src/App.jsx
@@ -0,0 +1,12 @@
+import Titulo from "./Titulo"
+
+export default function App() {
+
+  return (
+
+   <div>
+    <Titulo />
+   </div>
+  )
+};
+
---
+++ projetoreact1/src/Titulo.jsx
@@ -0,0 +1,9 @@
+export default function Titulo() {
+
+    return (
+        <>
+            <h1>Oi eu sou um title</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, culpa hic? Accusantium, earum repudiandae tenetur velit facilis maxime reiciendis sint tempora labore corporis soluta cum nisi. Maxime magni consectetur itaque?</p>
+        </>
+    )
+}
